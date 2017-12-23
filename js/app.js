1e431523cd7b704e27309bef42e514e70f852861
Saritha Kamath <sarithakamath24@gmail.com>
2017-12-23 08:16:30 -0800

    style: Format file content
    Formatted html and js file content for better readability.

--- js/app.js
+++ js/app.js
@@ -1,46 +1,46 @@
 function loadData() {
 
-    var $body = $('body');
-    var $heading = $('#heading');
-    var $nytHeaderElem = $('#nytimes-header');
-    var $nytElem = $('#nytimes-articles');
+  var $body = $('body');
+  var $heading = $('#heading');
+  var $nytHeaderElem = $('#nytimes-header');
+  var $nytElem = $('#nytimes-articles');
 
-    // clear out old data before new request
-    $heading.text ("");
-    $nytElem.text("");
+  // clear out old data before new request
+  $heading.text("");
+  $nytElem.text("");
 
-    // load streetview
-    var street = $('#street').val();
-    var city = $('#city').val();
-    var address = street + ', ' + city
+  // load streetview
+  var street = $('#street').val();
+  var city = $('#city').val();
+  var address = street + ', ' + city
 
-    $heading.text (street=="" ? city.toUpperCase(): address);
+  $heading.text(street == "" ? city.toUpperCase() : address);
 
-    var streetURL = "http://maps.googleapis.com/maps/api/streetview?size=600x300&location=" + address+ '';
-    $body.append('<img class="bgimg" src="'+streetURL+'">');
+  var streetURL = "http://maps.googleapis.com/maps/api/streetview?size=600x300&location=" + address + '';
+  $body.append('<img class="bgimg" src="' + streetURL + '">');
 
-    //AJAX request for NY times API
-    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="
-               + city +
-               "&sort=newest&api-key=9840a8412fbd4db997f582d96e756dce&begin_date=20170901" ;
+  //AJAX request for NY times API
+  var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" +
+    city +
+    "&sort=newest&api-key=9840a8412fbd4db997f582d96e756dce&begin_date=20170901";
 
-    $.getJSON(url)
-    .done(function(data) {
-       $nytHeaderElem.text("What's going on in "+ city);
-       articles = data.response.docs;
-       console.log(articles.length);
-       for (var i=0; i<articles.length; i++){
-           var article = articles[i];
-           $nytHeaderElem.append(
-            '<li class="article">'+'<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
-            '<p>'+article.snippet+'</p>'+
-            '</li>');
-           };
-    }).fail(function(e) {
-        $nytHeaderElem.text("New York Times articles could not be loaded.");
-  });
+  $.getJSON(url)
+    .done(function (data) {
+      $nytHeaderElem.text("What's going on in " + city);
+      articles = data.response.docs;
+      console.log(articles.length);
+      for (var i = 0; i < articles.length; i++) {
+        var article = articles[i];
+        $nytHeaderElem.append(
+          '<li class="article">' + '<a href="' + article.web_url + '">' + article.headline.main + '</a>' +
+          '<p>' + article.snippet + '</p>' +
+          '</li>');
+      };
+    }).fail(function (e) {
+      $nytHeaderElem.text("New York Times articles could not be loaded.");
+    });
 
-return false;
+  return false;
 };
 
-    $('#form-container').submit(loadData);
+$('#form-container').submit(loadData);
